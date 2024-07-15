6f7fd78266975ba70ac76745adf97523b8bad646
kkdresen <k.k.dresen@gmail.com>
2024-07-15 15:40:51 +0200

    ydata

--- Data/Cleaning.py
+++ Data/Cleaning.py
@@ -6,17 +6,17 @@ Data cleaning for adult
 import pandas as pd
 import os
 import numpy as np
-import pyRAPL
+# import pyRAPL
 
 # change directory
 os.chdir('Adult')
 
 def measure_cleaning():
     # measure energy consumption
-    pyRAPL.setup()
-    csv_output = pyRAPL.outputs.CSVOutput('../Energy/Measurement_cleaning.csv')
+    # pyRAPL.setup()
+    # csv_output = pyRAPL.outputs.CSVOutput('../Energy/Measurement_cleaning.csv')
 
-    with pyRAPL.Measurement('adult', output=csv_output):
+    # with pyRAPL.Measurement('adult', output=csv_output):
         # load data, use columns from adult.names file
         columns = ["age", "type_employer", "fnlwgt", "education", "education_num","marital", "occupation", "relationship", "race","sex","capital_gain", "capital_loss", "hr_per_week","country", "income"]
         adult = pd.read_csv("adult.data", sep=",\s",names=columns,na_values=["?"], engine='python')
@@ -56,10 +56,10 @@ def measure_cleaning():
         adult.to_csv('Adult_val.csv', index=False)
         # print("Validation set saved!")
 
-    csv_output.save()
+    # csv_output.save()
 
 for _ in range(10):
-	measure_cleaning()
+    measure_cleaning()
 
 print("Measurement of adult cleaning saved to csv.")
 
